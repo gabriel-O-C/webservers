9180bed7e687ebaa9d49176794bddf4b76531c4f
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-02-28 14:30:06 -0300

    filter bad words

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -32,6 +34,7 @@ func main() {
 
 	apiRouter.Get("/healthz", handlerReadiness)
 	apiRouter.Get("/reset", apiCfg.handlerReset)
+	apiRouter.Post("/validate_chirp", handlerChirpsValidate)
 
 	r.Mount("/api", apiRouter)
 	r.Mount("/admin", adminRouter)
@@ -45,4 +48,76 @@ func main() {
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	msg := params.Body
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: replaceBadWords(msg),
+	})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func replaceBadWords(msg string) string {
+	cursedWords := [3]string{"kerfuffle", "sharbert", "fornax"}
+
+	words := strings.Split(msg, " ")
+
+	returnMessage := []string{}
+
+	for _, word := range words {
+		if strings.ToLower(word) == cursedWords[0] || strings.ToLower(word) == cursedWords[1] || strings.ToLower(word) == cursedWords[2] {
+			word = "****"
+			returnMessage = append(returnMessage, word)
+		} else {
+			returnMessage = append(returnMessage, word)
+		}
+	}
+
+	return strings.Join(returnMessage, " ")
 }
